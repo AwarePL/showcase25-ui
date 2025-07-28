5377453b05ad2a8c30fe8a7f80fa13be1306f495
AwarePL <lukasz@korchowiec.pl>
2025-07-29 01:49:59 +0200

    setup linter

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import cypress from 'eslint-plugin-cypress'
+
+export default {
+  languageOptions: {
+    globals: {
+      browser: 'readonly',
+      es2021: 'readonly',
+    },
+  },
+  plugins: {
+    cypress: cypress,
+  },
+  rules: {
+    // Cypress-specific rules (only valid rules)
+    'cypress/no-unnecessary-waiting': 'error',
+    'cypress/no-force': 'warn',
+
+    // General code quality
+    'no-console': 'warn',
+    'no-unused-expressions': 'error',
+    'prefer-const': 'error',
+    'no-magic-numbers': ['error', { ignore: [0, 1] }],
+
+    // Style rules (aligned with Prettier)
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'prefer-template': 'error',
+    'no-trailing-spaces': 'error',
+  },
+}
