61e9b9b15f312f1baa46254578cd297e2d56807f
AwarePL <lukasz@korchowiec.pl>
2025-07-30 22:50:23 +0200

    add user generation and cookie handling

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,32 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import { faker } from '@faker-js/faker'
+
+Cypress.Commands.add(
+  'generateUserDetailsFixture',
+  { prevSubject: false },
+  () => {
+    // Generate user data with updated password and email logic
+    const user = {
+      email: faker.internet.email({ length: 20 }),
+      password: faker.internet.password({
+        length: 12,
+      }),
+      favoritePet: faker.animal.dog(),
+    }
+
+    // Create a timestamp for the filename
+    const timestamp = new Date().toISOString().replace(/[:.\s]/g, '-')
+    const filename = `generatedTestUser_${timestamp}.json`
+
+    // Save the user to a generated folder
+    const filePath = `cypress/fixtures/generated/${filename}`
+    cy.writeFile(filePath, user).then(() => {
+      // Return the user object for use in tests
+      return user
+    })
+  },
+)
+
+Cypress.Commands.add('setDismissCookies', { prevSubject: false }, () => {
+  cy.setCookie('cookieconsent_status', 'dismiss')
+  cy.setCookie('welcomebanner_status', 'dismiss')
+})
